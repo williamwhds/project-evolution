70a95aa4c6123298c1bcfabd7f7a817ca1070494
William Oliveira <202211240022@ifba.edu.br>
2024-10-03 20:49:13 -0300

    wip: detecting when an item is near

--- components/itemcomponent/item_detector.gd
+++ components/itemcomponent/item_detector.gd
@@ -3,5 +3,6 @@ extends Area2D
 # A collision shape must be attached for the detector to work
 @export var collision_shape : CollisionShape2D
 
-func _on_item_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_area_entered(area: Area2D) -> void:
+	if area.get_parent() is ItemComponent:
+		print("Got near an Item!")
