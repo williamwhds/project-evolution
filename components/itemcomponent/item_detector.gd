1c817b5a0588a281f11c4e4546c36553ab50df1d
William Oliveira <202211240022@ifba.edu.br>
2024-10-04 23:52:05 -0300

    wip: keep an array of nearby items
    todo: make it so the game prompts the user asking if they want to pick up the item. only make it so there's only one prompt at a time.

--- components/itemcomponent/item_detector.gd
+++ components/itemcomponent/item_detector.gd
@@ -2,7 +2,16 @@ extends Area2D
 
 # A collision shape must be attached for the detector to work
 @export var collision_shape : CollisionShape2D
+var nearby_items: Array = []
 
-func _on_area_entered(area: Area2D) -> void:
-	if area.get_parent() is ItemComponent:
-		print("Got near an Item!")
+func _on_item_entered(area: Area2D) -> void:
+	var item = area.get_parent() if area.get_parent() is ItemComponent else null
+	if item != null:
+		nearby_items.append(item)
+		print("Entered an item!")
+
+func _on_item_exited(area: Area2D) -> void:
+	var item = area.get_parent() if area.get_parent() is ItemComponent else null
+	if item != null:
+		nearby_items.erase(item)
+		print("Exited an item!")
