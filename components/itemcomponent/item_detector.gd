c9bf878c981ce147f171c845624fe1d7eb60d5a0
William Oliveira <202211240022@ifba.edu.br>
2024-10-07 12:23:28 -0300

    wip: formatting

--- components/itemcomponent/item_detector.gd
+++ components/itemcomponent/item_detector.gd
@@ -1,28 +1,30 @@
 extends Area2D
 
-# A collision shape must be attached for the detector to work
-@export var collision_shape : CollisionShape2D
+@export var collision_shape : CollisionShape2D # A collision shape must be attached for the detector to work
+@export var can_detect_owned_items: bool = false # This may be useful for making a "steal item" system.
 var nearby_items: Array = []
 
-# Signals for when an item is entered or exited.
-# These will also emit if the item has an owner.
-# This might be useful for making an "item steal" mechanic.
 signal item_entered(item: ItemComponent) 
 signal item_exited(item: ItemComponent)
+
+func _ready() -> void:
+	if collision_shape == null:
+		printerr("Item Detector: No collision shape attached!")
+		return
 
 func _on_item_entered(area: Area2D) -> void:
 	var item = area.get_parent() if area.get_parent() is ItemComponent else null
 	if item != null:
 		nearby_items.append(item)
 		item_entered.emit(item)
-		print("Entered an item!")
+		# print("Entered an item!")
 
 func _on_item_exited(area: Area2D) -> void:
 	var item = area.get_parent() if area.get_parent() is ItemComponent else null
 	if item != null:
 		nearby_items.erase(item)
 		item_exited.emit(item)
-		print("Exited an item!")
+		# print("Exited an item!")
 
 func get_first_nearby_item() -> ItemComponent:
 	if nearby_items.size() > 0:
